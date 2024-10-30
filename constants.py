c72e3748c1f7df8a05b19593f9623413b9a721a5
mdickson05 <124563538+mdickson05@users.noreply.github.com>
2024-10-30 18:03:06 +0800

    FIX: Moved constants into own file

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Constants
+
+# List of nucleotides: Adenine Guanine Thymine and Cytosine
+nucleotides = ["A", "G", "T", "C"]
+# Dictionary of complements: Maps each nucleotide to its complement
+nuc_complements = {"A" : "T", "G" : "C", "T" : "A", "C" : "G"}
